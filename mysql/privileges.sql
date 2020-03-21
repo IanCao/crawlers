28ec27f92a8b75b3a23a98d126769cc360e43abc
caoyixiong <caoyixiong@apache.org>
2020-03-21 23:01:49 +0800

    fix

--- mysql/privileges.sql
+++ mysql/privileges.sql
@@ -2,6 +2,6 @@ use mysql;
 select host, user from user;
 -- 因为mysql版本是5.7，因此新建用户为如下命令：
 -- create user root identified by '123456';
-grant all on house.* to root@'%' identified by '123456' with grant option;
+grant all on * to root@'%' identified by '123456' with grant option;
 -- 这一条命令一定要有：
 flush privileges;
